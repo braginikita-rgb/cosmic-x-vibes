2b58e5fbd9f4e3263f8b705f4dcab844eeff4e41
braginkita-rgb <braginikita@gmail.com>
2026-09-11 21:50:51 +0300

    Set home shop section coming soon

--- src/lib/i18n.tsx
+++ src/lib/i18n.tsx
@@ -44,7 +44,7 @@ const dict = {
     "ann.all": "Все даты",
 
     "merch.tag": "Мерч",
-    "merch.title": "Магазин",
+    "merch.title": "Coming soon",
     "merch.toShop": "В магазин",
     "merch.tee": "Футболка",
     "merch.hoodie": "Худи",
@@ -135,7 +135,7 @@ const dict = {
     "ann.all": "All dates",
 
     "merch.tag": "Merch",
-    "merch.title": "Shop",
+    "merch.title": "Coming soon",
     "merch.toShop": "To the shop",
     "merch.tee": "T-shirt",
     "merch.hoodie": "Hoodie",
